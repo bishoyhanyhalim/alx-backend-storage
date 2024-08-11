28db3777d84737d69bf28775c06c1fe86691efda
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-11 01:03:22 +0000

    task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- You should use attributes task
+-- Your script task 3
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
