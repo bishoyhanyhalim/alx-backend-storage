391ed92566388750fa66ee17f3de064aafb03b19
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-11 00:59:59 +0000

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- A SQL script 
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
